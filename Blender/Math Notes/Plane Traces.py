86bbce48f636d3be9a28c2bed53d7f43e71a93ec
L@T <4ON91@users.noreply.github.com>
2018-01-09 05:51:46 -0800

    Update Plane Traces.py

--- Blender/Math Notes/Plane Traces.py
+++ Blender/Math Notes/Plane Traces.py
@@ -14,7 +14,7 @@ import bpy
 import numpy as np
 from math import *
 from bpy.types import Operator
-from bpy.props import FloatVectorProperty
+from bpy.props import FloatVectorProperty, FloatProperty
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from mathutils import Vector
 
@@ -24,14 +24,45 @@ def add_object(self, context):
     edges = []
     faces = []
     
+    Eq = self.AxByCz
+    D = int(self.D)
+    Range = int(self.Range)
     #Where equation of the plane is
-    #-2*x + 3*y + 1*z - 6
-    for x in range(-6, 7, 1):
-        for z in range(-6, 7, 1):
-            y = (2*x)/3 - z/3 + 6/3
+    #-2x + 3y + 1z - 6
+    #-2x = -3y - 1z + 6
+    #x = (3/2)y + (1/2)z - (6/2)
+    # 3y = 2x - 1z + 6
+    # y = (2x/3) - (1z/3) + (6/3)
+    #Ax + By + Cz = D
+    #By = -Ax - Cz + D
+    #y = -(A/B)x - (C/B)z + (D/B)
+ 
+    """
+    for x in range(-D, D+1, 1):
+        for z in range(-D, D+1, 1):
+            y = (2*x)/3 - (1*z)/3 + 6/3
             verts.append(Vector((x, y, z)))
+    """ 
+    """
+    [0,1,5,6]
+    
+    """
+    for x in range(-Range, Range+1, 1):
+        for z in range(-Range, Range+1, 1):
+            y = ( -(Eq[0]*x)/Eq[1]) - (Eq[2]*z/Eq[1]) + D/Eq[1]
+            verts.append(Vector((x, y, z)))
+            #y = -x - z + 4
+            #1y = -(x/1) - (z/1) + 4
+    a = 1
+    c = Range*2+1
+    for x in range(1, len(verts)-(c)):
+        a = x
+        if(a%(c)==0):
+            a+=1
+        faces.append([a, a-1, a+c-1, a+c])
+        #faces.append([a+c, a+c-1, a-1,a  ])
         
-    mesh = bpy.data.meshes.new(name="New Object Mesh")
+    mesh = bpy.data.meshes.new(name="Plane Equation")
     mesh.from_pydata(verts, edges, faces)
     object_data_add(context, mesh, operator=self)
 
@@ -42,6 +73,23 @@ class OBJECT_OT_add_object(Operator, AddObjectHelper):
     bl_label = "Add Mesh Object"
     bl_options = {'REGISTER', 'UNDO'}
 
+    AxByCz = FloatVectorProperty(
+        name = "Plane Equation",
+        default = (3,-9, 2),
+        precision = 6,
+        description = 'The equation of a plane',)
+    
+    D = FloatProperty( 
+        name = "D", 
+        default = 9,
+        precision = 6,
+        description = 'Plane is equal to...',)
+        
+    Range = FloatProperty(
+        name = "Range",
+        default = 10,
+        precision = 6,
+        description = "How long the plane should extend to",)
     def execute(self, context):
 
         add_object(self, context)
